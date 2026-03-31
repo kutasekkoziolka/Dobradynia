1fd75a4549174f843a9d3753f477cb7a35a2e0ea
kutasekkoziolka <167017223+kutasekkoziolka@users.noreply.github.com>
2026-03-31 15:47:44 +0200

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -28,8 +28,8 @@ declare -A STOCK_IMAGES=(
 )
 
 # ── KernelSU defaults ──────────────────────────────────────────────
-KSU_DEFAULT_REPO="https://github.com/sidex15/KernelSU-Next.git"
-KSU_DEFAULT_BRANCH="legacy-susfs-v2"
+KSU_DEFAULT_REPO="https://github.com/kutasekkoziolka/KernelSU-Next.git"
+KSU_DEFAULT_BRANCH="pershoot-susfs-legacy"
 
 # ── Kernel settings ────────────────────────────────────────────────
 KERNEL_BRANCH="android11"
